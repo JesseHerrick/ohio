49df602d8e77be3e8a697c3341d63f374ffa3ece
Jesse Herrick <me@jesse.codes>
2017-05-30 09:04:36 -0400

    Create a (failing) test for searching for/by committee assignments

--- test/models/assignment_test.exs
+++ test/models/assignment_test.exs
@@ -3,4 +3,35 @@ defmodule Ohio.AssignmentTest do
 
   alias Ohio.Assignment
 
+  test "Assignment.find_by_committee/1 can find all legislators on a particular committee" do
+    committee = insert(:committee)
+
+    # Makes a new legislator and assigns them to the committee, `committee`.
+    legislators_on_committee = [
+      insert(:assignment, committee: committee).legislator,
+      insert(:assignment, committee: committee).legislator,
+      insert(:assignment, committee: committee).legislator,
+      insert(:assignment, committee: committee).legislator,
+      insert(:assignment, committee: committee).legislator
+    ]
+
+      # Should return a list of legislators on the committee.
+      assert] Assignment.find_by_committee(committee.id) == legislators_on_committee
+  end
+
+  test "Assignment.find_by_legislator/1 can find all committees assigned to a particular legislator" do
+    legislator = insert(:legislator)
+
+    # Makes a new committee and assigns our `legislator` to it.
+    committees_assigned_to_legislator = [
+      insert(:assignment, legislator: legislator).committee,
+      insert(:assignment, legislator: legislator).committee,
+      insert(:assignment, legislator: legislator).committee,
+      insert(:assignment, legislator: legislator).committee,
+      insert(:assignment, legislator: legislator).committee
+    ]
+
+
+    assert Assignment.find_by_legislator(legislator.id) == committees_assigned_to_legislator
+  end
 end
